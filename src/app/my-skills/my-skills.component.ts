edaae869c9b1decd8ff9f136f441e0408013d03d
Mabu4 <max.budziat@gmx.de>
2022-04-05 13:39:43 +0200

    added my skills and my work

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-my-skills',
   templateUrl: './my-skills.component.html',
-  styleUrls: ['./my-skills.component.scss']
+  styleUrls: ['./my-skills.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class MySkillsComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    this.loadCards();
+  }
+
+  images = 
+  [
+    'java-script.svg', 
+    'angular.png', 
+    'html-css.svg',
+    'scrum.svg',
+    'git.svg',
+    'design.svg',
+    'api.svg',
+    'test.svg',
+    'database.svg'
+  ];
+  descriptions = 
+  [
+    'Java script',
+    'Angular',
+    'HTML / CSS',
+    'SCRUM',
+    'Git',
+    'Design Thinking',
+    'Rest API',
+    'Test automation',
+    'Databases'
+  ];
+
+  cardContainer = '';
+
+  loadCards(){
+    for (let i = 0; i < this.images.length; i++) {
+      let image = this.images[i];
+      let description = this.descriptions[i];
+
+      this.cardContainer += /*html*/`
+      
+      <div class="skill-card-element">
+        <div class="skill-card-img-outer">
+          <img src="../../assets/img/${image}" alt="">
+        </div>
+        <p class="paragraph">
+          ${description}
+        </p>
+      </div>
+      `;
+    }
+
+    
+
   }
 
 }
--- src/app/my-work/my-work.component.ts
+++ src/app/my-work/my-work.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-my-work',
   templateUrl: './my-work.component.html',
-  styleUrls: ['./my-work.component.scss']
+  styleUrls: ['./my-work.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
+
 export class MyWorkComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    this.loadProjects();
   }
 
+  projectsContainer = '';
+
+  angular = 
+  [
+    {
+      'name': 'Portfolio',
+      'description': 'Personal Webpage',
+      'image': 'man.jpg'
+    },
+    {
+      'name': 'Join',
+      'description': 'Team Project',
+      'image': 'team.jpg'
+    }
+  ];
+  javascript = 
+  [
+    {
+      'name': 'El-Pollo-Loco',
+      'description': 'Jump and run game',
+      'image': 'elpolloloco.png'
+    },
+    {
+      'name': 'Quizapp',
+      'description': 'Developer Quiz',
+      'image': 'quizapp.png'
+    }
+  ];
+
+  projects = this.angular.concat(this.javascript);
+  end = 4;
+  start = 0;
+
+  loadProjects(){
+    console.log(this.projects);
+
+    for (let i = this.start; i < this.end; i++) {
+      let project = this.projects[i];
+      
+      this.projectsContainer += /*html*/`
+      <div class="project-element">
+        <img src="../../assets/img/projects/${project['image']}">
+        <div class="project-hover-overlay">
+          <h3 class="paragraph color-primary padding-bottom-xs">${project['name']}</h3>
+          <p class="paragraph padding-bottom-l">${project['description']}</p>
+          <a class="button-red">Try out</a>
+        </div>
+      </div>
+    `;
+    }
+  }
 }
+
